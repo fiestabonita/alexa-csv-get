3286c25a4ceb04b1f5bd01a3547aef1f0a4ff90f
fiestabonita <jeez@heaven.govment>
2015-01-31 16:53:28 -0600

    Update alexa.sh

--- alexa.sh
+++ alexa.sh
@@ -3,11 +3,12 @@ rm top-1m.csv.zip
 wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 unzip top-1m.csv.zip
 
+cat top-1m.csv | cut -d, -f2 | sed -r 's/\.[a-z\.]+$//' | awk ' !x[$0]++' > bigsites.txt
+# what the previous line is doing:
 # cat pipes the csv to cut
 # cut limits output to field 2, delimited by comma
 # sed removes the .com, .eu, .co.jp, .net, etc. from each line
 # awk removes duplicate lines
 # > redirects output to bigsites.txt
-cat top-1m.csv | cut -d, -f2 | sed -r 's/\.[a-z\.]+$//' | awk ' !x[$0]++' > bigsites.txt
 
 more bigsites.txt
